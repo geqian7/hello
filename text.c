2c3305e3290b54fe825bdf75bf172f76c5f4fa93
搁浅 <oneagain46@gmail.com>
2026-04-03 09:12:30 +0800

    hello

--- text.c
+++ text.c
@@ -913,3 +913,15 @@
 // int main()
 // {
 // }
+
+#include <stdio.h>
+#include "add.h"
+int main()
+{
+    int a = 0;
+    int b = 0;
+    scanf("%d%d", &a, &b);
+    int sum = add(a, b);
+    printf("%d", sum);
+    return 0;
+}
